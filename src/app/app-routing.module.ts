cbd806df9ec6c0061203f43f19ef6a6328fb25fc
MinjoyF <ghislainngabiapsi@gmail.com>
2022-09-28 02:02:59 +0200

    Coris added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ModelComponent } from './model/model.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'models/:p1' ,component:ModelComponent},
+  { path:'models/:p1/:p2' ,component:ModelComponent},
+ { path:'' ,redirectTo:'models/1/0', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StylisteService } from './styliste.service';
+import { Observable}  from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MJ-APP';
+export class AppComponent implements OnInit {
+
+public stylistes;
+
+constructor(private stylisteService: StylisteService ,private router:Router) {}
+
+  ngOnInit():void {
+
+    this.getStylistes();
+  }
+  private getStylistes() {
+    this.stylisteService.getResource("/api/stylistes")
+    .subscribe(data=>{ this.stylistes= data;
+    },err=>{
+      console.log(err);
+    })
+
+//
+
+
+  }
 }
+
+
+
+
+
